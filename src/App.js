59b17e58a7f6641f6132e22ed6db04eb77c4b4e1
Swathi <swathi.h@perfios.com>
2021-11-12 17:57:20 +0530

    ADDED: Path to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Payrollform from './components/payroll-form/Payrollform.jsx';
+import Home from './components/home/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Payrollform />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Payrollform />} />
         </Routes>
       </BrowserRouter>
     </div>
