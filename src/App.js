effbdacf295a264ab4a580bdd64443a2b55e5ce4
Swathi <swathi.h@perfios.com>
2021-11-13 07:06:18 +0530

    MODIFIED: Used switch instead of routes to navigate

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import Payrollform from './components/payroll-form/Payrollform.jsx';
 import Home from './components/home/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<Payrollform />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route exact path="/add"><Payrollform/></Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
