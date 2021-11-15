a2d84a438bfe26665433e764f3a515299399954f
Swathi <swathi.h@perfios.com>
2021-11-15 13:24:29 +0530

    MODIFIED: Navigating to form on clicking edit utton

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './display.scss';
 import EmployeeService from '../../services/employee-service';
 import deleteIcon from '../../assets/icons/delete-black-18dp.svg';
 import editIcon from '../../assets/icons/create-black-18dp.svg';
-import profile from '../../assets/profile-images/Ellipse3.png';
 
 
 const Display = (props) => {
@@ -17,26 +16,10 @@ const Display = (props) => {
         }).catch(error => {
             console.log("Error after ", error);
         });
-        window.location.replace("/");
+        window.location.replace("/home");
         // <Redirect to="/" />
     }
 
-    const update = (id) => {
-        let employeeData;
-        employeeService.getEmployee(id).then(data => {
-            employeeData = data.data;
-            localStorage.setItem("employeeData", JSON.stringify(employeeData));
-        }).catch(error => {
-            console.log("Error after ", error);
-        });
-        window.location.replace("/update");
-        // <Redirect to="/add" />
-        employeeService.updateEmployee(employeeData, id).then(data => {
-            console.log("Deleted data: ", data);
-        }).catch(error => {
-            console.log("Error after ", error);
-        })
-    }
     return (
         <table id="display" className="table">
             <tbody>
@@ -51,23 +34,21 @@ const Display = (props) => {
                 </tr>
                 {
                     props.employeeArray && props.employeeArray.map((element, index) => (
-                        
+
                         <tr key={index}>
-                            {console.log(element)}
                             <td>
                                 <img className="profile" id='image1' src={require('../../assets/profile-images/Ellipse1.png')} alt="profile" />
-                                {/* <img className="profile" alt="Profile" src={require('../../assets/profile-images/Ellipse -2.png')} /> */}
                             </td>
                             <td>{element.name}</td>
                             <td>{element.gender}</td>
-                            <td>{element.departmentValues && element.departmentValues.map(dept => (
+                            <td>{element.department && element.department.map(dept => (
                                 <div className="dept-label">{dept}</div>
                             ))}</td>
                             <td>{element.salary}</td>
                             <td>{element.startDate}</td>
                             <td>
                                 <img onClick={() => remove(element.id)} alt="delete" src={deleteIcon} />
-                                <img onClick={() => update(element.id)} alt="edit" src={editIcon} />
+                                <Link to={`/add/${element.id}`} > <img src={editIcon} alt="edit" /> </Link>
                             </td>
                         </tr>
                     ))
